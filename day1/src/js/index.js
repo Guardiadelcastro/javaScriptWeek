263dbe2d6a4a0892ce3fa8b09adcc9971bbb538e
Alejandro Castro <guardiadelcastro@gmail.com>
2018-10-08 20:33:18 +0100

    Finished dice throws

--- day1/src/js/index.js
+++ day1/src/js/index.js
@@ -1,1 +1,39 @@
 /*** index.js ***/
+var button1 = document.getElementById('button1');
+button1.onclick = function randomNum() {
+  var result = Math.floor(Math.random() * 6 + 1); 
+  console.log(result);
+  if (result === 7){
+    result = 6;
+  }
+  var diceNum = document.getElementById('number1');
+  diceNum.textContent = result;
+  
+  var results = [];
+  var results1 = document.getElementById('results1');
+  results1.push(result);
+  results1.textContent = results;
+};
+
+var button2 = document.getElementById('button2');
+button2.onclick = function randomNum() {
+  var result = Math.floor(Math.random() * 6 + 1); 
+  console.log(result);
+  if (result === 7){
+    result = 6;
+  }
+  var diceNum = document.getElementById('number2');
+  diceNum.textContent = result;
+};
+
+var button3 = document.getElementById('button3');
+button3.onclick = function randomNum() {
+  var result = Math.floor(Math.random() * 6 + 1); 
+  console.log(result);
+  if (result === 7){
+    result = 6;
+  }
+  var diceNum = document.getElementById('number3');
+  diceNum.textContent = result;
+};
+
